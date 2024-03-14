0286d7db1b6f317f510e07d758e018fd57990115
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-03-14 16:37:22 -0300

    Realizo Ejercicio 4

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'Use strict'
+"Use strict"
 
 // Ejercicio 1
 alert("un mensaje");
@@ -9,3 +9,8 @@ document.write("Hello World");
 // Ejercicio 3
 document.write("<br>");
 document.write("3 + 5 = ", 3+5);
+document.write("<br>");
+
+// Ejercicio 4
+let respuesta4 = prompt("Ingrese usuario");
+document.write ("Hola " + respuesta4);
