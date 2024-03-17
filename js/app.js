5ad35b1083267687e848405377b6d4bc56679f2e
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-03-17 00:08:41 -0300

    Realizo Ejercicio 5

--- js/app.js
+++ js/app.js
@@ -14,3 +14,9 @@ document.write("<br>");
 // Ejercicio 4
 let respuesta4 = prompt("Ingrese usuario");
 document.write ("Hola " + respuesta4);
+document.write("<br>");
+
+// Ejercicio 5
+let num1 = parseInt(prompt("Introduce el primer número"));
+let num2 = parseInt(prompt("Introduce el segundo número"));
+document.write("La suma es: ", num1 + num2);
